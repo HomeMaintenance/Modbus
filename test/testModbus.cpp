7b9f38236e5fb2f009a88cd29956d1103b330199
Planet-Moon <topwastaken@abwesend.de>
2022-02-15 00:58:06 +0100

    change test name

---
+++ test/testModbus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "TestDevice.h"
+#include <chrono>
+#include <thread>
+
+int main(int argc, char **argv){
+    TestDevice testDevice("192.168.178.113",502, false);
+    testDevice.intRegister.getValue();
+    testDevice.intRegister.getValue();
+    testDevice.intRegister.getValue(true);
+    std::this_thread::sleep_for(std::chrono::seconds(static_cast<unsigned long>(5)));
+    testDevice.intRegister.getValue();
+    testDevice.intRegister.getValue();
+    return 0;
+}
