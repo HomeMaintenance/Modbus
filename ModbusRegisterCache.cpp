e5dd422c177c4091a3bc8b9db2295d21b97dbdeb
Planet-Moon <topwastaken@abwesend.de>
2022-02-15 19:47:56 +0100

    cache is dirty when last read failed

--- ModbusRegisterCache.cpp
+++ ModbusRegisterCache.cpp
@@ -22,6 +22,9 @@ int RegisterCache::register_read_status(){
 }
 
 bool RegisterCache::dirty() const{
+    if(_register_read_status != data.size())
+        return true;
+
     std::clock_t time_now{std::clock()};
     bool result = (time + max_age) < time_now;
     return result;
